6543dd1e59330d96faf4fa7a3de76f5eb9432984
Mehedee-Hassan <razin.meha@gmail.com>
2014-09-28 20:37:34 +0600

    update 1

---
+++ problemSet2_prob2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace problemSet2_prob2
+{
+    partial class FindMinAndMaxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.thirdNumberTextBox = new System.Windows.Forms.TextBox();
+            this.fifthNumberTextBox = new System.Windows.Forms.TextBox();
+            this.fourthNumberTextBox = new System.Windows.Forms.TextBox();
+            this.maxShowLabel = new System.Windows.Forms.Label();
+            this.minShowLabel = new System.Windows.Forms.Label();
+            this.maxValueLabel = new System.Windows.Forms.Label();
+            this.minValueLabel = new System.Windows.Forms.Label();
+            this.firstNumberLabel = new System.Windows.Forms.Label();
+            this.secondNumberLabel = new System.Windows.Forms.Label();
+            this.thirdNumberLabel = new System.Windows.Forms.Label();
+            this.fourthNumberLabel = new System.Windows.Forms.Label();
+            this.fifthNumberLabel = new System.Windows.Forms.Label();
+            this.findButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(93, 12);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(125, 20);
+            this.firstNumberTextBox.TabIndex = 0;
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(93, 38);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(125, 20);
+            this.secondNumberTextBox.TabIndex = 1;
+            // 
+            // thirdNumberTextBox
+            // 
+            this.thirdNumberTextBox.Location = new System.Drawing.Point(93, 64);
+            this.thirdNumberTextBox.Name = "thirdNumberTextBox";
+            this.thirdNumberTextBox.Size = new System.Drawing.Size(125, 20);
+            this.thirdNumberTextBox.TabIndex = 2;
+            // 
+            // fifthNumberTextBox
+            // 
+            this.fifthNumberTextBox.Location = new System.Drawing.Point(93, 116);
+            this.fifthNumberTextBox.Name = "fifthNumberTextBox";
+            this.fifthNumberTextBox.Size = new System.Drawing.Size(125, 20);
+            this.fifthNumberTextBox.TabIndex = 3;
+            // 
+            // fourthNumberTextBox
+            // 
+            this.fourthNumberTextBox.Location = new System.Drawing.Point(93, 90);
+            this.fourthNumberTextBox.Name = "fourthNumberTextBox";
+            this.fourthNumberTextBox.Size = new System.Drawing.Size(125, 20);
+            this.fourthNumberTextBox.TabIndex = 4;
+            // 
+            // maxShowLabel
+            // 
+            this.maxShowLabel.AutoSize = true;
+            this.maxShowLabel.Location = new System.Drawing.Point(62, 183);
+            this.maxShowLabel.Name = "maxShowLabel";
+            this.maxShowLabel.Size = new System.Drawing.Size(27, 13);
+            this.maxShowLabel.TabIndex = 5;
+            this.maxShowLabel.Text = "Max";
+            // 
+            // minShowLabel
+            // 
+            this.minShowLabel.AutoSize = true;
+            this.minShowLabel.Location = new System.Drawing.Point(172, 183);
+            this.minShowLabel.Name = "minShowLabel";
+            this.minShowLabel.Size = new System.Drawing.Size(24, 13);
+            this.minShowLabel.TabIndex = 6;
+            this.minShowLabel.Text = "Min";
+            // 
+            // maxValueLabel
+            // 
+            this.maxValueLabel.AutoSize = true;
+            this.maxValueLabel.Location = new System.Drawing.Point(62, 217);
+            this.maxValueLabel.Name = "maxValueLabel";
+            this.maxValueLabel.Size = new System.Drawing.Size(0, 13);
+            this.maxValueLabel.TabIndex = 7;
+            // 
+            // minValueLabel
+            // 
+            this.minValueLabel.AutoSize = true;
+            this.minValueLabel.Location = new System.Drawing.Point(172, 217);
+            this.minValueLabel.Name = "minValueLabel";
+            this.minValueLabel.Size = new System.Drawing.Size(0, 13);
+            this.minValueLabel.TabIndex = 8;
+            // 
+            // firstNumberLabel
+            // 
+            this.firstNumberLabel.AutoSize = true;
+            this.firstNumberLabel.Location = new System.Drawing.Point(20, 15);
+            this.firstNumberLabel.Name = "firstNumberLabel";
+            this.firstNumberLabel.Size = new System.Drawing.Size(64, 13);
+            this.firstNumberLabel.TabIndex = 9;
+            this.firstNumberLabel.Text = "1st Number:";
+            // 
+            // secondNumberLabel
+            // 
+            this.secondNumberLabel.AutoSize = true;
+            this.secondNumberLabel.Location = new System.Drawing.Point(16, 38);
+            this.secondNumberLabel.Name = "secondNumberLabel";
+            this.secondNumberLabel.Size = new System.Drawing.Size(68, 13);
+            this.secondNumberLabel.TabIndex = 10;
+            this.secondNumberLabel.Text = "2nd Number:";
+            // 
+            // thirdNumberLabel
+            // 
+            this.thirdNumberLabel.AutoSize = true;
+            this.thirdNumberLabel.Location = new System.Drawing.Point(19, 64);
+            this.thirdNumberLabel.Name = "thirdNumberLabel";
+            this.thirdNumberLabel.Size = new System.Drawing.Size(65, 13);
+            this.thirdNumberLabel.TabIndex = 11;
+            this.thirdNumberLabel.Text = "3rd Number:";
+            // 
+            // fourthNumberLabel
+            // 
+            this.fourthNumberLabel.AutoSize = true;
+            this.fourthNumberLabel.Location = new System.Drawing.Point(19, 93);
+            this.fourthNumberLabel.Name = "fourthNumberLabel";
+            this.fourthNumberLabel.Size = new System.Drawing.Size(65, 13);
+            this.fourthNumberLabel.TabIndex = 12;
+            this.fourthNumberLabel.Text = "4th Number:";
+            // 
+            // fifthNumberLabel
+            // 
+            this.fifthNumberLabel.AutoSize = true;
+            this.fifthNumberLabel.Location = new System.Drawing.Point(16, 119);
+            this.fifthNumberLabel.Name = "fifthNumberLabel";
+            this.fifthNumberLabel.Size = new System.Drawing.Size(65, 13);
+            this.fifthNumberLabel.TabIndex = 13;
+            this.fifthNumberLabel.Text = "5th Number:";
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(80, 277);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(75, 23);
+            this.findButton.TabIndex = 14;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // FindMinAndMaxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 341);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.fifthNumberLabel);
+            this.Controls.Add(this.fourthNumberLabel);
+            this.Controls.Add(this.thirdNumberLabel);
+            this.Controls.Add(this.secondNumberLabel);
+            this.Controls.Add(this.firstNumberLabel);
+            this.Controls.Add(this.minValueLabel);
+            this.Controls.Add(this.maxValueLabel);
+            this.Controls.Add(this.minShowLabel);
+            this.Controls.Add(this.maxShowLabel);
+            this.Controls.Add(this.fourthNumberTextBox);
+            this.Controls.Add(this.fifthNumberTextBox);
+            this.Controls.Add(this.thirdNumberTextBox);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Name = "FindMinAndMaxForm";
+            this.Text = "Find Max & Min";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.TextBox thirdNumberTextBox;
+        private System.Windows.Forms.TextBox fifthNumberTextBox;
+        private System.Windows.Forms.TextBox fourthNumberTextBox;
+        private System.Windows.Forms.Label maxShowLabel;
+        private System.Windows.Forms.Label minShowLabel;
+        private System.Windows.Forms.Label maxValueLabel;
+        private System.Windows.Forms.Label minValueLabel;
+        private System.Windows.Forms.Label firstNumberLabel;
+        private System.Windows.Forms.Label secondNumberLabel;
+        private System.Windows.Forms.Label thirdNumberLabel;
+        private System.Windows.Forms.Label fourthNumberLabel;
+        private System.Windows.Forms.Label fifthNumberLabel;
+        private System.Windows.Forms.Button findButton;
+    }
+}
+
---
+++ problemSet2_prob2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problemSet2_prob2
+{
+    public partial class FindMinAndMaxForm : Form
+    {
+        public FindMinAndMaxForm()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            int max ;
+            int min;
+            min = max = Convert.ToInt32(firstNumberTextBox.Text);
+
+
+            List<int> numbers = new List<int>();
+
+
+
+            int nowAtTextBox = 0;
+            foreach (Control control in this.Controls)
+            {
+                if (control.GetType() == typeof (TextBox))
+                {
+                    //numbers.Add(Convert.ToInt32((control as TextBox).Text));
+
+                    nowAtTextBox = Convert.ToInt32((control as TextBox).Text);
+
+                    if (max < nowAtTextBox)
+                    {
+                        max = nowAtTextBox;
+                    }
+
+                    if (min > nowAtTextBox)
+                    {
+                        min = nowAtTextBox;
+                    }
+                    
+                }
+            }
+
+
+           
+
+            maxValueLabel.Text = max.ToString();
+            minValueLabel.Text = min.ToString();
+
+        }
+    }
+}
